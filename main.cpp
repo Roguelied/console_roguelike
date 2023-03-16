3e20a1c576b635eda775dbcba3d9b6e8a26f6484
Roguelied <110971527+Roguelied@users.noreply.github.com>
2023-03-16 19:27:38 +0300

    Init

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <cstring>
+
+class Item
+{
+private:
+    std::string Name;
+
+};
+
+class Sword : public Item
+{
+private:
+    int Damage;
+    int Durability = 100;
+public:
+    void Attack(int enemy) {
+        //do something
+        this -> Durability--;
+    }
+
+};
+
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
 }
