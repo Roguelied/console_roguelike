ed407b962709178a2722ab25e8c5038998db4eab
Vasiliy Davydov <davvvas@mail.ru>
2023-03-19 11:54:48 +1000

    немножко начал писать логику

--- main.cpp
+++ main.cpp
@@ -1,15 +1,44 @@
-
 #include "Utility.h"
-
 #include <string>
 #include <iostream>
 #include <windows.h>
+using namespace std;
 
+class Player {
+    friend void Player_class (Player &prime);
+
+private:
+    int health = 100;
+    int damage = 10;
+    int stamina = 100;
+
+public:
+    string name;
+    int chosen_class;
+
+    void Print(){
+        cout << "Имя " << name << "\nКласс " << chosen_class << endl << endl;
+    }
+
+};
+
+void Player_class (Player &prime) {
+    if (prime.chosen_class == 0){
+        prime.health *= 1.5;
+        prime.damage *= 2;
+    } else {
+        prime.damage *= 1.5;
+        prime.stamina *= 2;
+    }
+
+};
 
 int main() {
-    SetConsoleOutputCP( 65001 );
-    for (;;) {PlayCharacterAttackAnimation(); }
+    setlocale(LC_ALL, "ru");
+    Player player;
+    cout << "Введите имя игрока:\n"; cin >> player.name;
+    cout << "Выберите класс персонажа (0 - воин, 1 - лучник):\n"; cin >> player.chosen_class;
+    player.Print();
 
-    int a;
-    std::cin >> a;
+    return 0;
 }
