1cdd019fc9c3014224a8ed885ada4caf4b75ae7d
notfounddd <zapara222@bk.ru>
2023-03-19 23:52:05 +1000

    JackOfAllTrades Character - Rogue Monk Archer Swordsman Character movement Sprite fix Level set up

--- AnimationAndSprites.h
+++ AnimationAndSprites.h
@@ -1,9 +1,17 @@
 #ifndef CONSOLE_ROGUELIKE_CPP_ANIMATIONANDSPRITES_H
 #define CONSOLE_ROGUELIKE_CPP_ANIMATIONANDSPRITES_H
 #include "Utility.h"
-
+#include "Character.h"
 
 void PlayKnightAttackAnimation();
 void PlayArcherAttackAnimation();
+
+class Animation {
+public:
+    void Play(Player Player, Enemy Enemy) {
+
+    }
+};
+
 
 #endif //CONSOLE_ROGUELIKE_CPP_ANIMATIONANDSPRITES_H
--- LevelDesign.h
+++ LevelDesign.h
@@ -4,10 +4,17 @@
 
 class GameLevel
 {
+private:
+
 public:
+
     string GameLevelArray[29][119];
     GameLevel(int RoomType);
+
     void DrawGameLevel();
+    void DrawLine(int x1, int y1, int x2, int y2);
+    void DrawFrame(int x1, int y1, int x2, int y2);
+    void DrawFill(int x1, int y1, int x2, int y2);
 
 };
 #endif //CONSOLE_ROGUELIKE_CPP_LEVELDESIGN_H
